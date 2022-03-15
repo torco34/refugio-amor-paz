6bd1fe7ad2b324f8ac24b1fef355d95cedeaae92
Torcoroma <torcoromaarias@gmail.com>
2022-03-15 17:56:52 -0500

    err

---
+++ index.js
@@ -0,0 +1,5 @@
+const app = require("./router/app");
+const port = 8080;
+app.listen(port, () => {
+  console.log("puerto 8080, refugio amor");
+});
--- src/services/sendgrid.js
+++ src/services/sendgrid.js
@@ -1,4 +1,5 @@
 const sgMail = require("@sendgrid/mail");
+const { json } = require("body-parser");
 const config = require("../config");
 sgMail.setApiKey(config.SENDGRID_API_KEY);
 
